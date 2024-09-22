5cf353d575999470384af36a471dfed71dc374fb
unknown <alvaroreynosorx@gmail.com>
2024-09-21 22:01:03 -0300

    ej1

--- Unidad 2/EJERCICIOS BOOL,SWITCH,FOR,DO/ej1.c
+++ Unidad 2/EJERCICIOS BOOL,SWITCH,FOR,DO/ej1.c
@@ -1,13 +1,12 @@
+//verificar si el numero ingresado es impar o par
 #include <stdio.h>
 #include <stdlib.h>
 
 void main()
 {
   int numero;
-  const
-  printf ("Ingrese numero\n");
+  printf("Ingrese numero\n");
   scanf("%d",&numero);
-  int numero=num+1;
   if (numero % 2==0)
    {
     printf ("el numero %d es par",numero);
