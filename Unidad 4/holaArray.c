743f2731fe4f6954484f5c4c2c28ac402b7e9541
unknown <alvaroreynosorx@gmail.com>
2024-10-21 20:43:50 -0300

    first commit

--- Unidad 4/holaArray.c
+++ Unidad 4/holaArray.c
@@ -1,36 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+//Ingrese un nombre y lo muestre en pantalla n veces
 void main()
 {
-  // char hola[]={'H','O','L','A',' ','M','U','N','D','O'};
-
-  // for (int i = 0; i <=9; i++)
-  // {
-  //   printf("%c",hola[i]);
-  // }
-  // char nombre[5][40];
-  // for (int i = 0; i <=4; i++)
-  // {
-  //   printf("Ingrese el nombre %d: \n",i+1);
-  //   scanf("%s",&nombre);
-  // }
-  // for ( int i = 0; i <5; i++)
-  // {
-  //   printf("Los nombres son %s",nombre[i]);
-  // }
-  char nombre[17];
-  printf("Ingrese su nombre: ");
-  fgets(nombre, 17,stdin);
-  printf("%s",nombre);
-
-  // if (strcpy(nombre, "Alvaro Reynoso")==0)
-  // {
-  //   printf("Bienvenido  %s\n", nombre);
-  // }
-  // else
-  // {
-  //   printf("nombre no compatible con el usuario");
-  // }
+  char nombre[5][40];
+  for (int i = 0; i <=4; i++)
+  {
+    printf("Ingrese el nombre %d: \n",i+1);
+    gets(nombre[i]);
+  }
+  for ( int i = 0; i <5; i++)
+  {
+    printf("Los nombres son %s \n",nombre[i]);
+  }
 }
